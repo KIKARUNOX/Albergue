db2e25aae25694d51a182601eaed998fb38f2087
KikaruNox <Brandon.rodriguezgrijalba@ucr.ac.cr>
2026-03-26 16:22:19 -0600

    logo

--- src/components/organisms/AppHeader.tsx
+++ src/components/organisms/AppHeader.tsx
@@ -5,8 +5,12 @@ export default function AppHeader({ onLogout }: AppHeaderProps) {
   return (
     <header className="app-header">
       <div>
-        <p className="eyebrow">Codigo316</p>
+        <img src="/codigo3_16.PNG" alt="Logo de Codigo316" className="logo" />
+        <p className="eyebrow"> Codigo  3:16</p>
+
         <h1 className="app-title">Panel de Administracion</h1>
+
+
       </div>
       <Button onClick={onLogout}>Cerrar sesion</Button>
     </header>
